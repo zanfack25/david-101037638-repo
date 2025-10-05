38e9f2d35c77a2807c44d321d5a886c28f782913
zanfack25 <zanfackdave@gmail.com>
2025-10-05 01:42:20 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
@@ -15,7 +16,7 @@ app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
-    app.listen(8080, () => console.log('Listening on port 8080'));
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }).catch((err) => {
     console.error(err);
     process.exit(1);
